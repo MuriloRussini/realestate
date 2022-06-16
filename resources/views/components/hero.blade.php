93525753bd82636f1b82488922b0fdf83b33efad
MuriloRussini <murilorussini@usp.br>
2022-06-16 18:19:21 -0300

    commit on 'dev'

---
+++ resources/views/components/hero.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.main')
+
+@section ('content')
+  <div class="hero">
+    <div class="hero__images">
+      <div class="hero__image">
+        <img src="https://images.dwell.com/photos-6311371353569939456/6739650785763569664-large/modern-architectural-style-with-warm-finishes-to-compliment-mountain-setting.jpg" class="hero__image-img" alt="">
+
+        <div class="hero__info">
+          <span class="hero__subtitle">Residential</span>
+          <span class="hero__title">Residential</span>
+          <a href="#" class="hero__info-link">Residential</a>
+        </div>
+      </div>
+
+      <div class="hero__image">
+        <img src="https://cdn.onekindesign.com/wp-content/uploads/2017/01/Contemporary-Home-Design-Vertical-Arts-Architecture-01-1-Kindesign.jpg" class="hero__image-img" alt="">
+
+        <div class="hero__info">
+          <span class="hero__subtitle">Residential</span>
+          <span class="hero__title">Residential</span>
+          <a href="#" class="hero__info-link">Residential</a>
+        </div>
+      </div>
+
+      <div class="hero__image">
+        <img src="https://www.luxury-architecture.net/wp-content/uploads/2018/03/1521770360-1408-wildwood-main.jpg" class="hero__image-img" alt="">
+
+        <div class="hero__info">
+          <span class="hero__subtitle">Residential</span>
+          <span class="hero__title">Residential</span>
+          <a href="#" class="hero__info-link">Residential</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="hero__indicator">
+      <div class="hero__indicator-item">
+        <span class="hero__indicator-number">01</span>
+        <span class="hero__indicator-text">Creampie Villa</span>
+      </div>
+      <div class="hero__indicator-item">
+        <span class="hero__indicator-number">02</span>
+        <span class="hero__indicator-text">Creampie Villa</span>
+      </div>
+      <div class="hero__indicator-item">
+        <span class="hero__indicator-number">03</span>
+        <span class="hero__indicator-text">Creampie Villa</span>
+      </div>
+    </div>
+  </div>
+@endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.main')
 
 @section('content')
-  <h1>Creampie though</h1>
+  @include('components.hero')
 @endsection
