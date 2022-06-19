4c16fc69da6f007ce4389c4d65d945949f1beec1
MuriloRussini <murilorussini@usp.br>
2022-06-19 16:01:23 -0300

    y

--- resources/views/pages/listings.blade.php
+++ resources/views/pages/listings.blade.php
@@ -5,7 +5,35 @@
   <div class="listings-city">
     <img src="https://cdn.onekindesign.com/wp-content/uploads/2017/01/Contemporary-Home-Design-Vertical-Arts-Architecture-01-1-Kindesign.jpg" alt="" class="listings-city__img">
 
-    <h1>South Beach</h1>
+    <h1 class="listings-city__title">South Beach</h1>
+  </div>
+
+  <div class="listings-filter">
+    <div class="listings-filter__wrapper">
+      <div class="listings-filter__option">
+        Any Price <i class="fa-solid fa-caret-down"></i>
+      </div>
+  
+      
+      <div class="listings-filter__option">
+        All Beds <i class="fa-solid fa-caret-down"></i>
+      </div>
+  
+      
+      <div class="listings-filter__option">
+        Hometype <i class="fa-solid fa-caret-down"></i>
+      </div>
+  
+      
+      <div class="listings-filter__option">
+        More <i class="fa-solid fa-caret-down"></i>
+      </div>
+  
+      
+      <div class="listings-filter__button">
+        Search
+      </div>
+    </div>
   </div>
 </div>
 @endsection
