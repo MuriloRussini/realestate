cbb33303bfc6719d429c1b52ae36c6b5a30728f5
MuriloRussini <murilorussini@usp.br>
2022-10-08 23:17:01 -0300

    s

--- resources/views/pages/single-listing.blade.php
+++ resources/views/pages/single-listing.blade.php
@@ -5,7 +5,12 @@
 @section('content')
 <div class="single-listing-page">
   <div class="listing-top">
-    <img src="https://cdn.onekindesign.com/wp-content/uploads/2017/01/Contemporary-Home-Design-Vertical-Arts-Architecture-01-1-Kindesign.jpg" alt="" class="listing-top__img">
+    @foreach ($photos as $photo)
+        @if ($photo->featured)
+        <img src="http://localhost:3000/img/{{ $photo->name }}" alt="" class="listing-top__img">
+        @endif
+    @endforeach
+
     <div class="listing-top__form-wrapper">
       <div class="container">
         <form action="" class="listing-top__form">
@@ -45,18 +50,18 @@
     <div class="container">
       <div class="row">
         <div class="col-md-7">
-          <h1>23 Grand Ave<br>
-          Miami, FL 33456</h1>
+          <h1>{{ $listing->adress }} {{ $listing->adress2 }}<br>
+            {{ $listing->city }}, {{ $listing->state }} {{ $listing->zipcode }}</h1>
           <div class="listing-info__details">
-            <span class="listing-info__details-text"><i class="fa-solid fa-bed"></i> 4</span>
-            <span class="listing-info__details-text"><i class="fa-solid fa-bath"></i> 3</span>
-            <span class="listing-info__details-text"><i class="fa-solid fa-ruler"></i> 2440 SQFT</span>
+            <span class="listing-info__details-text"><i class="fa-solid fa-bed"></i> {{ $listing->bedrooms }}</span>
+            <span class="listing-info__details-text"><i class="fa-solid fa-bath"></i> {{ $listing->bathrooms }}</span>
+            <span class="listing-info__details-text"><i class="fa-solid fa-ruler"></i> {{ $listing->squarefootage }} SQFT</span>
           </div>
         </div>
         <div class="col-md-5">
           <span class="listing-info__agent-title">Agent</span>
-          <span class="listing-info__agent-name">John Smith</span>
-          <p class="listing-info__agent">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur soluta rem recusandae velit voluptatem nisi voluptatum ullam veniam, magni in? Adipisci ipsa nisi quas molestias animi architecto quibusdam dolor voluptatibus.</p>
+          <span class="listing-info__agent-name">{{ $listing->user->name }}</span>
+          <p class="listing-info__agent">TRY TO ADD PROFILE FOR EACH AGENT</p>
         </div>
       </div>
     </div>
@@ -67,27 +72,31 @@
         <div class="col-md-7">
           <div class="listing-extras__details">
             <h2>More Info</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati quibusdam tempore ad neque veritatis eos rerum repellendus odit ipsa sed accusantium quod soluta consequatur molestiae quos provident vitae, laborum natus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque quam saepe iure eum, a labore sapiente mollitia temporibus est nostrum, perferendis, voluptates voluptatibus quod odit obcaecati error at officiis. Temporibus?</p>
+            <p>{{ $listing->description }}</p>
             <h3>Details</h3>
             <ul>
-              <li>Test</li>
-              <li>Test</li>
-              <li>Test</li>
-              <li>Test</li>
-              <li>Test</li>
-              <li>Test</li>
-              <li>Test</li>
-              <li>Test</li>
-              <li>Test</li>
-              <li>Test</li>
-              <li>Test</li>
+              <li>This could be a tag</li>
+              <li>This could be a tag</li>
+              <li>This could be a tag</li>
+              <li>This could be a tag</li>
+              <li>This could be a tag</li>
+              <li>This could be a tag</li>
+              <li>This could be a tag</li>
+              <li>This could be a tag</li>
+              <li>This could be a tag</li>
+              <li>This could be a tag</li>
+              <li>This could be a tag</li>
             </ul>
           </div>
         </div>
         <div class="col-md-5">
-          <div class="listing-extras_gallery">
+          <div class="listing-extras__gallery">
             <h2>Images</h2>
-            <img src="https://cdn.onekindesign.com/wp-content/uploads/2017/01/Contemporary-Home-Design-Vertical-Arts-Architecture-01-1-Kindesign.jpg" alt="">
+            @foreach ($photos as $photo)
+              @if (!$photo->featured)
+              <img src="http://localhost:3000/img/{{ $photo->name }}" alt="">
+              @endif
+            @endforeach
           </div>
         </div>
       </div>
